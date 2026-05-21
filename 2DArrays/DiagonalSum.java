3d2d24749ac331ade75ecc995d1adb90b4e9876f
Sainath <sainath4501@gmail.com>
2026-05-21 18:38:35 +0530

    add

--- 2DArrays/DiagonalSum.java
+++ 2DArrays/DiagonalSum.java
@@ -16,12 +16,28 @@ public class DiagonalSum {
         return sum;
     }
 
+   public static int diagonalSum1(int matrix[][]){
+        int sum=0;
+        for(int i=0;i<matrix.length;i++){
+            //Primary Diagonal
+             sum+=matrix[i][i];
+
+             //Secondary Diagonal
+             if(i!=matrix[i][matrix.length-1-i]){
+                sum+=matrix[i][matrix.length-1-i];
+             }
+
+        }
+        return sum;
+   }
+
     public static void main(String args[]){
         int matrix[][]={{1,2,3,4},
                         {5,6,7,8},
                         {9,10,11,12},
                         {13,14,15,16}}; 
-        int Sum=diagonalSum(matrix);
+        // int Sum=diagonalSum(matrix);
+        int Sum=diagonalSum1(matrix);
         System.out.println(Sum);                
     }
 
---
+++ 2DArrays/SearchInSortMatrix.java
@@ -0,0 +1,30 @@
+public class SearchInSortMatrix {
+    
+   public static boolean searchinSortedmatrix(int matrix[][],int key){
+        int row=0;
+        int col=matrix[0].length-1;
+
+        while (row<=matrix.length && col>=0) {
+            
+            if(key==matrix[row][col]){
+                System.out.print("Element found at : ("+row+","+col+")");
+                return true;
+            }
+            else if(key<matrix[row][col]){
+                col--;
+            }else{
+                row++;
+            }
+        }
+        System.out.println("Element not found...!");
+        return false;
+   }
+    public static void main(String args[]){
+        int matrix[][]={{1,2,3,4},
+                        {5,6,7,8},
+                        {9,10,11,12},
+                        {13,14,15,16}}; 
+
+        searchinSortedmatrix(matrix, 16);              
+    }
+}
