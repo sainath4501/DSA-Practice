468f80df85d78fb533794a9df933754973496d05
Sainath <sainath4501@gmail.com>
2026-05-23 20:26:48 +0530

    add

--- Strings/UpperCase.java
+++ Strings/UpperCase.java
@@ -1,3 +1,26 @@
 public class UpperCase {
-    
+
+    public static  String UpperCase1(String str){
+        StringBuilder sb=new StringBuilder("");
+        char ch=Character.toUpperCase(str.charAt(0));
+        sb.append(ch);
+        for(int i=1;i<str.length();i++){
+            if(str.charAt(i)==' '&& i<str.length()-1){
+                sb.append(str.charAt(i));
+                i++;
+                sb.append(Character.toUpperCase(str.charAt(i)));
+            }
+            else{
+                sb.append(str.charAt(i));
+            }
+        }
+        return sb.toString();
+
+
+    }
+    public static void main(String args[]){
+        String str="i am sainath";
+        String Upper=UpperCase1(str);
+        System.out.println(Upper);
+    }
 }
