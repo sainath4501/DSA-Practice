71ebfbd448af7fbc95a84b384d7f16bbf658e573
sainath4501 <sainath4501@gmail.com>
2026-09-10 12:54:40 +0530

    add

---
+++ ArrayList/Create.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Create {
+
+    public static void main(String[] args) {
+        
+        ArrayList<Integer> List=new  ArrayList<>();
+    }
+}
---
+++ ArrayList/Operations.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Operations {
+    public static void main(String[] args) {
+        ArrayList <Integer> List = new ArrayList<>();
+        List.add(10);
+        List.add(16);
+        System.out.println(List);
+    }
+}
